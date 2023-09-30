35a416a9a428e5e8c662fd98d395bca901f7ac78
albedim <dimaio.albe@gmail.com>
2023-09-30 13:16:07 +0200

    [improvement] - user id's don't increment

--- src/model/entity/User.py
+++ src/model/entity/User.py
@@ -1,16 +1,18 @@
 import datetime
 from src.configuration.config import sql
+from src.utils.Utils import Utils
 
 
 class User(sql.Model):
     __tablename__ = 'users'
-    user_id: int = sql.Column(sql.Integer, primary_key=True)
+    user_id: int = sql.Column(sql.String(14), primary_key=True, autoincrement=False)
     name: str = sql.Column(sql.String(40), nullable=True)
     email: str = sql.Column(sql.String(40), nullable=False)
     password: str = sql.Column(sql.String(40), nullable=False)
     created_on: datetime.date = sql.Column(sql.Date, nullable=True)
 
     def __init__(self, name, email, password):
+        self.user_id = Utils.createCode(14)
         self.email = email
         self.name = name
         self.password = password
